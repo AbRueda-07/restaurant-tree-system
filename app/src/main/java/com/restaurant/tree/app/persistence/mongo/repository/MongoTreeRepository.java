f3bc5d60bfcc1f4a4e3e8dc6f0d094872c2da977
J-Oslee19 <jcermenop2miumg.edu.gt>
2026-05-21 01:59:59 -0600

    feat: adapt MongoDB persistence to TreeRepository architecture

---
+++ app/src/main/java/com/restaurant/tree/app/persistence/mongo/repository/MongoTreeRepository.java
@@ -0,0 +1,10 @@
+package com.restaurant.tree.app.persistence.mongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.restaurant.tree.app.persistence.mongo.document.MongoTreeDocument;
+
+@Repository
+public interface MongoTreeRepository extends MongoRepository<MongoTreeDocument, String> {
+}
